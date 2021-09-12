fe5616f4836ff7db986cf30bfe61405c97caf20f
KirillKrutski <60190964+KirillKrutski@users.noreply.github.com>
2021-09-12 11:51:13 +0300

    Update Main.java

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -6,10 +6,12 @@ public class Main {
         MoneyBox moneyBox2 = new MoneyBox(); // не задаем ни один параметр
 //        int x = moneyBox2.weight;
 //        System.out.println(x);
-        HeavyBox heavyBox1 = new HeavyBox(100 , 150,150,100);
+       HeavyBox heavyBox1 = new HeavyBox(100 , 150,150,100);
         int newWeight = heavyBox1.weight * 2;
         heavyBox1.weight = newWeight;
-//        int newHeight = heavyBox1.height * 2; // .height выделяется красным
-//        heavyBox1.height
+        heavyBox1.setHeight(heavyBox1.getHeight()*2);
+        System.out.println(heavyBox1.getHeight());
+        heavyBox1.setHeight(-10); // присваиваем значению ширина отрицательное значение
+        System.out.println(heavyBox1.getHeight());
     }
 }
